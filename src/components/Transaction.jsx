4cea463e4d0a9c030b93d4a06e33485db0cee8cc
Shivam Sharma <meshivam81@gmail.com>
2023-07-01 20:24:04 +0530

    add GlobalState and color

---
+++ src/components/Transaction.jsx
@@ -0,0 +1,32 @@
+import PropTypes from 'prop-types';
+import { useContext } from "react"
+import { GlobalContext } from "../context/GlobalState"
+
+const Transaction = ({ transaction }) => {
+ const {  deleteTransaction } = useContext(GlobalContext);
+
+ const sign = transaction.amount < 0 ? '-' : '+';
+
+
+  return (
+    <> 
+    <li className={transaction.amount < 0 ? 'minus' : 'plus'} 
+    key={transaction.id}>{transaction.text}<span>{sign}${Math.abs(transaction.amount)}</span>
+    <button onClick={()=>  deleteTransaction(transaction.id)} className="delete-btn">X</button>
+    </li>
+
+    </>
+  )
+}
+
+// Add prop validations if necessary
+Transaction.propTypes = {
+    transaction: PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      text: PropTypes.string.isRequired,
+      amount: PropTypes.number.isRequired,
+       
+    }).isRequired,
+  };
+
+export default Transaction
---
+++ src/context/GlobalState.jsx
@@ -0,0 +1,65 @@
+import PropTypes from 'prop-types';
+import { createContext, useReducer } from "react";
+import AppReducer from './AppReducer'
+
+// initial state
+const initialState = {
+    transactions: [
+        {
+            id: 1,
+            text: 'Flower',
+            amount: -20
+        },
+
+        {
+            id: 2,
+            text: 'Salary',
+            amount: 300
+        },
+
+        {
+            id: 3,
+            text: 'Book',
+            amount: -10
+        },
+
+        {
+            id: 4,
+            text: 'Camera',
+            amount: 150
+        }
+    ]
+
+}
+
+// Create context
+export const GlobalContext = createContext(initialState)
+
+// Provider component to wrap all component
+export const GlobalProvider = ({ children }) =>{
+ const [state, dispatch] = useReducer(AppReducer, initialState);
+
+ // Action
+  const deleteTransaction = (id) =>{
+    dispatch({
+        type: 'DELETE_TRANSACTION',
+        payload: id
+    })
+  }
+
+ return (
+
+ <GlobalContext.Provider value={{
+    transactions: state.transactions,
+    deleteTransaction
+}}>
+    
+    {children}</GlobalContext.Provider>
+ 
+ );
+}
+
+// Add prop validations if necessary
+GlobalProvider.propTypes = {
+    children: PropTypes.node.isRequired
+  };
