90631cf530fe59d457499dd7c5ea1ec32ff9a214
Shivam Sharma <meshivam81@gmail.com>
2023-07-15 21:51:17 +0530

    let's keep transaction History

--- src/components/TransactionList.jsx
+++ src/components/TransactionList.jsx
@@ -9,7 +9,7 @@ const TransactionList = () => {
   
   return (
     <>
-      <h3>History</h3>
+      <h3>Transaction History</h3>
       <ul id="list" className="list">
         {transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction} />))}
       </ul>
